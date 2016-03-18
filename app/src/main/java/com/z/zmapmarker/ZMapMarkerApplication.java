2b8e8cfdd3be33c42f39ad5711f2119482fc7f5a
jason@0xng.com <jason@0xng.com>
2016-03-18 21:44:52 +0800

    add the static function to enable getting context all over the app

--- app/src/main/java/com/z/zmapmarker/ZMapMarkerApplication.java
+++ app/src/main/java/com/z/zmapmarker/ZMapMarkerApplication.java
@@ -1,16 +1,22 @@
 package com.z.zmapmarker;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.baidu.mapapi.SDKInitializer;
 
 public class ZMapMarkerApplication extends Application {
 
+    private static Context context;
     @Override
     public void onCreate() {
         super.onCreate();
         // 在使用 SDK 各组间之前初始化 context 信息，传入 ApplicationContext
         SDKInitializer.initialize(this);
+        context = getApplicationContext();
+    }
+    public static Context getContext(){
+        return context;
     }
 
 }
